3da7a9c33e1e9b0004fab1d40503d9c16dd989a0
bretelphick <elphicb1@tcnj.edu>
2022-03-22 21:24:49 -0400

    added code for local scrathc handle

--- active_learning/slurm_handling.py
+++ active_learning/slurm_handling.py
@@ -1,6 +1,5 @@
 
-
-
+node_root = "/local/scratch"
 
 def move_output(experiment_parameters, user_path, job_id_list):
     
@@ -10,3 +9,12 @@ def move_output(experiment_parameters, user_path, job_id_list):
         shutil.move(source_dir + job_id, user_path)
     
     experiment_parameters["output_root"] = user_path
+
+ job_id_list = []
+try:
+    job_id_list.append(os.environ["SLURM_JOB_ID"])
+    user_path = experiment_parameters["output_root"]
+    experiment_parameters["output_root"] = config.node_path
+except:
+    #print("Running locally.")
+    pass
